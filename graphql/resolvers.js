402bc62fa30243bfa87758c5ddbf6b8017ebff58
smkim89 <kiak357@gmail.com>
2018-07-25 23:55:17 +0900

    배열 추가.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,35 @@
-const Person = {
+const Eunmi = {
     name : "Eunmi",
     age : "29",
     gender : "F"
 };
 
+const People = [
+    {
+        id : 0,
+        name : "Eunmi",
+        age : "29",
+        gender : "F"
+    },
+    {
+        id : 1,
+        name : "SungMin",
+        age : "30",
+        gender : "M"
+    },
+    {
+        id : 3,
+        name : "OK",
+        age : "29",
+        gender : "F"
+    }
+
+];
+
 const resolvers = {
     Query : {
-        person : () => Person
+        people : () => People,
+        person : () => Eunmi
     }
 }
 
