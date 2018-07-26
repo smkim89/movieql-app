b9ff070127f63eae036cb51e3aad0184c26e7418
smkim89 <kiak357@gmail.com>
2018-07-26 18:07:50 +0900

    axios를 이용한 api 통신

--- graphql/api.js
+++ graphql/api.js
@@ -1,16 +1,63 @@
-import fetch from 'node-fetch';
-const API = "https://yts.am/api/v2/list_movies.json";
+// import fetch from 'node-fetch';
+import axios from 'axios';
 
-export const getMovie = (limit, rating) => {
-    let REQUEST_URL = API +'?';
-    if(limit>0){
-        REQUEST_URL += `limit=${limit}&`;
-    }
-    if(rating>0){
-        REQUEST_URL += `minimum_rating=${rating}&`;
-    }
-    console.log(REQUEST_URL);
-    return fetch(`${REQUEST_URL}`)  //`${}` String으로 변환하는 것.
-    .then(res => res.json())
-    .then(json => json.data.movies);
-}
+const BASE_URL = "https://yts.am/api/v2";
+const LIST_URL = `${BASE_URL}/list_movies.json`;
+const MOVIE_DETAIL_URL = `${BASE_URL}/movie_details.json`;
+const SUGGESTIONS_URL = `${BASE_URL}/movie_suggestions.json`;
+
+// export const getMovie = (limit, rating) => {
+//     let REQUEST_URL = API +'?';
+//     if(limit>0){
+//         REQUEST_URL += `limit=${limit}&`;
+//     }
+//     if(rating>0){
+//         REQUEST_URL += `minimum_rating=${rating}&`;
+//     }
+//     console.log(REQUEST_URL);
+//     return fetch(`${REQUEST_URL}`)  //`${}` String으로 변환하는 것.
+//     .then(res => res.json())
+//     .then(json => json.data.movies);
+// }
+
+
+export const getMovie = async (limit, rating) => {
+    const {
+        data : {
+            data : {movies}
+        }
+    } = await axios (LIST_URL, {
+        params : {
+            limit : limit,
+            minimum_rating : rating
+        }
+    });
+    return movies;
+};
+
+export const getMovieDetail = async id => {
+    const {
+        data : {
+            data : {movie}
+        }
+    } = await axios (MOVIE_DETAIL_URL, {
+        params : {
+            movie_id : id
+        }
+    });
+    return movie;
+};
+
+export const getMovieSuggestions = async id => {
+    const {
+        data : {
+            data : {movies}
+        }
+    } = await axios (SUGGESTIONS_URL, {
+        params : {
+            movie_id : id
+        }
+    });
+
+    return movies;
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,10 @@
-import {getMovie} from './api';
+import {getMovie, getMovieDetail, getMovieSuggestions} from './api';
 
 const resolvers = {
     Query : {
-        movies : (_, {limit, rating}) => getMovie(limit, rating)
-
+        movies : (_, {limit, rating}) => getMovie(limit, rating),
+        movieDetail : (_, { id }) => getMovieDetail(id),
+        movieSuggestions : (_, { id }) => getMovieSuggestions(id)
         //( _ , args ) , ( _ , {id} ) 같은의미 args가 json으로 받아드림
     }
 }
